b63c6f467d551725f2090c0dbeffcf7be9cd33b4
quilerks <idjsUYJ79K$ds>
2022-10-20 14:31:52 +0300

    add input validation

---
+++ DEV/jQuery/input-validation/input-validation.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function () {
+    console.log('x5');
+    jQuery('form input[name="phone"]').on("click keyup change blur input paste", function () {
+        let _this = jQuery(this),
+            value = _this.val();
+        //только цифры и знак +
+        _this.val(value.replace(/[^\d\+]/g, ''))
+
+        //только цифры и знак + и знак скобок
+        //_this.val(value.replace(/[^\d\(\)\+]/g, ''))
+
+        //только цифры и знак + и знак скобок и пробел
+        //_this.val(value.replace(/[^\d\(\)\+ ]/g, ''))
+
+        //только цифры и знак + и знак скобок и пробел и -
+        //_this.val(value.replace(/[^\d\(\)\-\+ ]/g, ''))
+    });
+
+    jQuery('form input[name="uname"], form input[name="your-name"], form input[name="name"]').on("click keyup change blur input paste", function () {
+        let _this = jQuery(this),
+            value = _this.val();
+        //только кириллица и латиница и пробел и знак -
+        _this.val(value.replace(/[^a-zA-Zа-яёА-ЯЁ -]/u, ''))
+    });
+
+});
