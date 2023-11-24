28cac77ea4c7b0e25223d63c2195f164fcfa1b74
quilerks <quilerks@gmail.com>
2023-11-24 17:49:12 +0300

    update script for deploy

--- DEV/MODX/Ubuntu/modx-ubuntu.sh
+++ DEV/MODX/Ubuntu/modx-ubuntu.sh
@@ -12,4 +12,12 @@ add-apt-repository ppa:ondrej/php
 apt install -y php7.4-common php7.4-cli php7.4-curl php7.4-json php7.4-gd php7.4-mysql php7.4-xml php7.4-zip php7.4-fpm php7.4-mbstring php7.4-bcmath php-pear
 
 # Ставим mariadb (он же в прошлом mysql). В инструкции было раньше 10.3
-apt install mariadb-server-10.6
+apt install -y mariadb-server-10.6
+
+#Добавляем нового пользователя в систему, под которым будет работать наш проект
+groupadd www-site
+useradd -d /home/site -s /bin/bash -g www-site site
+usermod -a -G www-site site
+usermod -a -G sudo site
+passwd site
+#password
