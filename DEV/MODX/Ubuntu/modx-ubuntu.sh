e78c2786531cfbd9cac881da260997ce9734110d
quilerks <quilerks@gmail.com>
2023-11-24 16:18:46 +0300

    add sh script for deploy modx site

---
+++ DEV/MODX/Ubuntu/modx-ubuntu.sh
@@ -0,0 +1,15 @@
+#Ubuntu 22
+# Обновляем пакеты системы
+apt-get update
+apt-get upgrade
+
+#ставим nginx
+apt install -y nginx
+
+# Установка без указания версии php (можно явно указать у отдельных пакетов php7.4)
+apt -y install software-properties-common
+add-apt-repository ppa:ondrej/php
+apt install -y php7.4-common php7.4-cli php7.4-curl php7.4-json php7.4-gd php7.4-mysql php7.4-xml php7.4-zip php7.4-fpm php7.4-mbstring php7.4-bcmath php-pear
+
+# Ставим mariadb (он же в прошлом mysql). В инструкции было раньше 10.3
+apt install mariadb-server-10.6
