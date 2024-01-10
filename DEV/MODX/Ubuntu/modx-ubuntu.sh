f82ffe048d0624352fc13deb29d526c3ff14d12c
quilerks <quilerks@gmail.com>
2024-01-10 14:43:36 +0300

    add package php7.4-intl

--- DEV/MODX/Ubuntu/modx-ubuntu.sh
+++ DEV/MODX/Ubuntu/modx-ubuntu.sh
@@ -9,7 +9,7 @@ apt install -y nginx
 # Установка без указания версии php (можно явно указать у отдельных пакетов php7.4)
 apt install -y software-properties-common
 add-apt-repository ppa:ondrej/php
-apt install -y php7.4-common php7.4-cli php7.4-curl php7.4-json php7.4-gd php7.4-mysql php7.4-xml php7.4-zip php7.4-fpm php7.4-mbstring php7.4-bcmath php-pear
+apt install -y php7.4-common php7.4-cli php7.4-curl php7.4-json php7.4-gd php7.4-mysql php7.4-xml php7.4-zip php7.4-fpm php7.4-mbstring php7.4-bcmath php7.4-intl php-pear
 
 # Ставим mariadb (он же в прошлом mysql). В инструкции было раньше 10.3
 apt install -y mariadb-server-10.6
